7344f2bb2e8b9af754b32f06f3d330fd790b2ef6
TimothyWang56 <timothywang56@gmail.com>
2021-01-14 21:28:42 -0500

    layout basic components in landing page

--- src/screen/landingpage/LandingPage.tsx
+++ src/screen/landingpage/LandingPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './LandingPage.scss'
 import {useHistory} from "react-router-dom";
 import Button from "../components/Button/Button";
 
@@ -6,8 +7,19 @@ const LandingPage = () => {
   const history = useHistory();
 
   return (
-    <div>
-      <Button text={"Try it out!"} onClick={() => history.push("/application")}/>
+    <div className="landing-page-wrapper">
+      <div className="landing-content"> 
+        <h1>PV2</h1>
+        <hr />
+        <div>Helping homeowners make economically wise decisions on residential solar PV systems.</div>
+        <hr />
+        <div>PV2 uses the E3 API developed at NIST to run an economic analysis of rooftop solar photovoltaics.</div>
+        <hr/>
+        <Button text={"Try it out!"} onClick={() => history.push("/application")}/>
+      </div>
+      <div className="landing-image">
+        {/* todo: figure out what to show for the image */}
+      </div>
     </div>
   );
 }
