28d5cd3e093cda2f2b760cf34ce92caa5bcc213f
TimothyWang56 <timothywang56@gmail.com>
2021-01-14 21:27:55 -0500

    remove index.css file (replaced with main.scss)

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-import './index.css';
+import './main.scss';
 import reportWebVitals from './reportWebVitals';
 
 import LandingPage from "./screen/LandingPage/LandingPage";
