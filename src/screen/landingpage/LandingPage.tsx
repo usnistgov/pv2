846fe63c249f603003ab0084ad399686a86e5e71
TimothyWang56 <timothywang56@gmail.com>
2021-01-18 14:53:34 -0500

    add temporary nist logo and header component

--- src/screen/landingpage/LandingPage.tsx
+++ src/screen/landingpage/LandingPage.tsx
@@ -3,25 +3,29 @@ import './LandingPage.scss'
 import {useHistory} from "react-router-dom";
 import Button from "../../components/Button/Button";
 import LandingImage from '../../assets/images/landing-image.png';
+import Header from "../../components/Header/Header";
 
 const LandingPage = () => {
   const history = useHistory();
 
   return (
-    <div className="landing-page-wrapper">
-      <div className="landing-content"> 
-        <h1>PV2</h1>
-        <hr />
-        <div>Helping homeowners make economically wise decisions on residential solar PV systems.</div>
-        <hr />
-        <div>PV2 uses the E3 API developed at NIST to run an economic analysis of rooftop solar photovoltaics.</div>
-        <hr/>
-        <Button text={"Try it out!"} onClick={() => history.push("/application")}/>
+    <>
+      <Header />
+      <div className="landing-page-wrapper">
+        <div className="landing-content"> 
+          <h1>PV2</h1>
+          <hr />
+          <div>Helping homeowners make economically wise decisions on residential solar PV systems.</div>
+          <hr />
+          <div>PV2 uses the E3 API developed at NIST to run an economic analysis of rooftop solar photovoltaics.</div>
+          <hr/>
+          <Button text={"Try it out!"} onClick={() => history.push("/application")}/>
+        </div>
+        <div className="landing-image">
+          <img src={LandingImage} alt={"landing page"} />
+        </div>
       </div>
-      <div className="landing-image">
-        <img src={LandingImage} alt={"landing page"} />
-      </div>
-    </div>
+    </>
   );
 }
 
