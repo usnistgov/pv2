d857cb2bf6c217692044a5d21b7ebcec2963d512
TimothyWang56 <timothywang56@gmail.com>
2021-01-19 00:35:24 -0500

    test landing page button exists

---
+++ src/__tests__/LandingPage.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import LandingPage from '../screen/LandingPage/LandingPage'
+import '@testing-library/jest-dom'
+
+describe("<LandingPage />", () => {
+  test('landing page should have button', async () => {
+    const { findByTestId } = renderLandingPage()
+
+    const landingPage = await findByTestId("landing-page")
+    const button = await findByTestId("button")
+
+    expect(landingPage).toContainElement(button)
+  })
+})
+
+function renderLandingPage() {
+  return render(<LandingPage />)
+}
