c882dbee0385fda31011cc63f6fa09cda9b3d166
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-01-19 14:35:39 -0500

    Converted ramining components to functional components redo

--- src/screen/application/Application.tsx
+++ src/screen/application/Application.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {ReactElement} from "react";
+
+import {Provider} from 'react-redux';
+import {Container, createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
 import StepperNav from "./components/StepperNav";
 import StepperPage from "./components/StepperPage";
 import SrecForm from "./pages/SrecForm";
 import SolarSystemForm from "./pages/SolarSystemForm";
 import ElectricalRateForm from "./pages/ElectricalRateForm";
 import AddressForm from "./pages/AddressForm";
-import {Provider} from 'react-redux';
-
-import {Container, createMuiTheme, MuiThemeProvider} from "@material-ui/core";
 import {rootStore} from "./ApplicationStore";
+
 
 export const theme = createMuiTheme({
     palette: {
@@ -27,31 +29,30 @@ export const theme = createMuiTheme({
     }
 })
 
-class Application extends React.Component {
-    render(): React.ReactNode {
-        return (
-            <Provider store={rootStore}>
-                <MuiThemeProvider theme={theme}>
-                    <Container>
-                        <StepperNav>
-                            <StepperPage label={"Address"}>
-                                <AddressForm/>
-                            </StepperPage>
-                            <StepperPage label={"Electrical Rate"}>
-                                <ElectricalRateForm/>
-                            </StepperPage>
-                            <StepperPage label={"Solar PV System"}>
-                                <SolarSystemForm/>
-                            </StepperPage>
-                            <StepperPage label={"SREC"}>
-                                <SrecForm/>
-                            </StepperPage>
-                        </StepperNav>
-                    </Container>
-                </MuiThemeProvider>
-            </Provider>
-        );
-    }
+/*
+ * Wrapper component for the main form application.
+ */
+export default function Application(): ReactElement {
+    return (
+        <Provider store={rootStore}>
+            <MuiThemeProvider theme={theme}>
+                <Container>
+                    <StepperNav>
+                        <StepperPage label={"Address"}>
+                            <AddressForm/>
+                        </StepperPage>
+                        <StepperPage label={"Electrical Rate"}>
+                            <ElectricalRateForm/>
+                        </StepperPage>
+                        <StepperPage label={"Solar PV System"}>
+                            <SolarSystemForm/>
+                        </StepperPage>
+                        <StepperPage label={"SREC"}>
+                            <SrecForm/>
+                        </StepperPage>
+                    </StepperNav>
+                </Container>
+            </MuiThemeProvider>
+        </Provider>
+    );
 }
-
-export default Application;
--- src/screen/application/pages/ElectricalRateForm.tsx
+++ src/screen/application/pages/ElectricalRateForm.tsx
@@ -1,136 +1,92 @@
-import {Component} from "react";
+import {ReactElement} from "react";
+
 import {Box} from "@material-ui/core";
-import MaterialHeader from "../components/MaterialHeader";
-import "./ElectricalRateForm.css"
-import FormField from "../components/FormField";
 import * as Yup from "yup";
-import {RootState} from "../ApplicationStore";
-import {connect} from "react-redux";
-import {
-    annualConsumptionSlice,
+
+import FormField from "../components/FormField";
+import MaterialHeader from "../components/MaterialHeader";
+import FormSelect from "../components/FormSelect";
+import {createNumberSlice, createStringSlice, useReduxGetSet} from "../Utils";
+
+import "./ElectricalRateForm.css"
+
+
+// Redux string slices
+export const [
     electricalCompanyNameSlice,
+    netMeteringFeedTariffSlice
+] = [
+    "electricalCompanyName",
+    "netMeteringFeedTariff"
+].map(createStringSlice);
+
+// Redux number slices
+export const [
+    annualConsumptionSlice,
+    monthlyFlatRateChargeSlice,
     electricUnitPriceSlice,
     excessGenerationUnitPriceSlice,
-    monthlyFlatRateChargeSlice,
-    netMeteringFeedTariffSlice,
     pvGridConnectionRateSlice
-} from "./ElectricalRateFormReducer";
-import {ReduxGetSet} from "../Utils";
-import FormSelect from "../components/FormSelect";
-
-interface StateProps {
-    electricalCompanyName: ReduxGetSet<string>,
-    annualConsumption: ReduxGetSet<number>,
-    monthlyFlatRateCharge: ReduxGetSet<number>,
-    electricUnitPrice: ReduxGetSet<number>,
-    excessGenerationUnitPrice: ReduxGetSet<number>,
-    pvGridConnectionRate: ReduxGetSet<number>,
-    netMeteringFeedTariff: ReduxGetSet<string>,
-}
-
-class ElectricalRateForm extends Component<StateProps, never> {
-    render() {
-        return (
-            <Box>
-                <MaterialHeader text={"Electrical Rate Information"}/>
-                <Box className={"electrical-form-container"}>
-                    <FormField label={"Electricity Utility Company"}
-                               schema={Yup.string()}
-                               value={this.props.electricalCompanyName}/>
-                    <FormField label={"Annual Consumption"}
-                               schema={Yup.number().required()}
-                               value={this.props.annualConsumption}
-                               endAdornment={"kWH"}
-                               type={"number"}/>
-                    <FormField label={"Monthly Flat Rate Charge"}
-                               schema={Yup.number().required()}
-                               value={this.props.monthlyFlatRateCharge}
-                               startAdornment={"$"}
-                               type={"number"}/>
-                    <FormField label={"Electricity Unit Price"}
-                               schema={Yup.number().required()}
-                               value={this.props.electricUnitPrice}
-                               endAdornment={"$/kWH"}
-                               type={"number"}/>
-                    <FormSelect label={"Net Metering or Feed In Tariff (FiT)"}
-                                value={this.props.netMeteringFeedTariff}
-                                options={[
-                                    "Net Metering Tariff",
-                                    "Feed in Tariff (Gross Metering)"
-                                ]}/>
-                    <FormField label={"Excess Generation / FiT Unit Price"}
-                               schema={Yup.number().required()}
-                               value={this.props.excessGenerationUnitPrice}
-                               endAdornment={"$/kWH"}
-                               type={"number"}/>
-                    <FormField label={"PV Grid Connection Rate (Monthly)"}
-                               schema={Yup.number().required()}
-                               value={this.props.pvGridConnectionRate}
-                               endAdornment={"$/kWH"}
-                               type={"number"}/>
-                </Box>
-            </Box>
-        );
-    }
-}
+] = [
+    "annualConsumption",
+    "monthlyFlatRateCharge",
+    "electricUnitPrice",
+    "excessGenerationUnitPrice",
+    "pvGridConnectionRate"
+].map(createNumberSlice);
 
 /*
- * Redux mappings and export
+ * Displays the electrical rate form.
  */
-function mapStateToProps(state: RootState) {
-    return {
-        electricalCompanyName: () => state.electricalCompanyName,
-        annualConsumption: () => state.annualConsumption,
-        monthlyFlatRateCharge: () => state.monthlyFlatRateCharge,
-        electricUnitPrice: () => state.electricUnitPrice,
-        excessGenerationUnitPrice: () => state.excessGenerationUnitPrice,
-        pvGridConnectionRate: () => state.pvGridConnectionRate,
-        netMeteringFeedTariff: () => state.netMeteringFeedTariff,
-    }
-}
+export default function ElectricalRateForm(): ReactElement {
+    // Redux state objects
+    const electricalCompanyName = useReduxGetSet<string>("electricalCompanyName", electricalCompanyNameSlice);
+    const netMeteringFeedTariff = useReduxGetSet<string>("netMeteringFeedTariff", netMeteringFeedTariffSlice);
+    const annualConsumption = useReduxGetSet<number>("annualConsumption", annualConsumptionSlice);
+    const monthlyFlatRateCharge = useReduxGetSet<number>("monthlyFlatRateCharge", monthlyFlatRateChargeSlice);
+    const electricUnitPrice = useReduxGetSet<number>("electricUnitPrice", electricUnitPriceSlice);
+    const excessGenerationUnitPrice = useReduxGetSet<number>("excessGenerationUnitPrice", excessGenerationUnitPriceSlice);
+    const pvGridConnectionRate = useReduxGetSet<number>("pvGridConnectionRate", pvGridConnectionRateSlice);
 
-const mapDispatchToProps = {
-    electricalCompanyName: (s: string) => electricalCompanyNameSlice.actions.set(s),
-    annualConsumption: (n: number) => annualConsumptionSlice.actions.set(n),
-    monthlyFlatRateCharge: (n: number) => monthlyFlatRateChargeSlice.actions.set(n),
-    electricUnitPrice: (n: number) => electricUnitPriceSlice.actions.set(n),
-    excessGenerationUnitPrice: (n: number) => excessGenerationUnitPriceSlice.actions.set(n),
-    pvGridConnectionRate: (n: number) => pvGridConnectionRateSlice.actions.set(n),
-    netMeteringFeedTariff: (s: string) => netMeteringFeedTariffSlice.actions.set(s),
+    return (
+        <Box>
+            <MaterialHeader text={"Electrical Rate Information"}/>
+            <Box className={"electrical-form-container"}>
+                <FormField label={"Electricity Utility Company"}
+                           schema={Yup.string()}
+                           value={electricalCompanyName}/>
+                <FormField label={"Annual Consumption"}
+                           schema={Yup.number().required()}
+                           value={annualConsumption}
+                           endAdornment={"kWH"}
+                           type={"number"}/>
+                <FormField label={"Monthly Flat Rate Charge"}
+                           schema={Yup.number().required()}
+                           value={monthlyFlatRateCharge}
+                           startAdornment={"$"}
+                           type={"number"}/>
+                <FormField label={"Electricity Unit Price"}
+                           schema={Yup.number().required()}
+                           value={electricUnitPrice}
+                           endAdornment={"$/kWH"}
+                           type={"number"}/>
+                <FormSelect label={"Net Metering or Feed In Tariff (FiT)"}
+                            value={netMeteringFeedTariff}
+                            options={[
+                                "Net Metering Tariff",
+                                "Feed in Tariff (Gross Metering)"
+                            ]}/>
+                <FormField label={"Excess Generation / FiT Unit Price"}
+                           schema={Yup.number().required()}
+                           value={excessGenerationUnitPrice}
+                           endAdornment={"$/kWH"}
+                           type={"number"}/>
+                <FormField label={"PV Grid Connection Rate (Monthly)"}
+                           schema={Yup.number().required()}
+                           value={pvGridConnectionRate}
+                           endAdornment={"$/kWH"}
+                           type={"number"}/>
+            </Box>
+        </Box>
+    );
 }
-
-const mergeProps = (stateProps: any, dispatchProps: any, ownProps: any) => {
-    return {
-        electricalCompanyName: {
-            get: stateProps.electricalCompanyName,
-            set: dispatchProps.electricalCompanyName,
-        },
-        annualConsumption: {
-            get: stateProps.annualConsumption,
-            set: dispatchProps.annualConsumption,
-        },
-        monthlyFlatRateCharge: {
-            get: stateProps.monthlyFlatRateCharge,
-            set: dispatchProps.monthlyFlatRateCharge,
-        },
-        electricUnitPrice: {
-            get: stateProps.electricUnitPrice,
-            set: dispatchProps.electricUnitPrice,
-        },
-        excessGenerationUnitPrice: {
-            get: stateProps.excessGenerationUnitPrice,
-            set: dispatchProps.excessGenerationUnitPrice,
-        },
-        pvGridConnectionRate: {
-            get: stateProps.pvGridConnectionRate,
-            set: dispatchProps.pvGridConnectionRate,
-        },
-        netMeteringFeedTariff: {
-            get: stateProps.netMeteringFeedTariff,
-            set: dispatchProps.netMeteringFeedTariff,
-        },
-        ...ownProps,
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(ElectricalRateForm);
