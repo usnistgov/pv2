79b8fb2fb294ea829d154046f83010410377a91f
TimothyWang56 <timothywang56@gmail.com>
2021-02-01 21:33:35 -0500

    make basic collapsable container component

---
+++ src/screen/application/components/CollapseContainer/CollapseContainer.tsx
@@ -0,0 +1,32 @@
+import {ReactElement, useState} from "react";
+
+import {Box, Collapse} from "@material-ui/core";
+import "./CollapseContainer.scss"
+import {ArrowDropDown, ArrowDropUp} from "@material-ui/icons"
+
+export interface CollapseContainerProps {
+  text: string
+}
+
+/*
+ * A collapsable container
+ */
+export default function CollapseContainer(props: React.PropsWithChildren<CollapseContainerProps>): ReactElement {
+  const [open, setOpen] = useState(false)
+
+  const handleOnClick = () => {
+    setOpen((prev) => !prev)
+  }
+
+  return (
+    <Box className={"collapse-container"}>
+      <div className="collapse-container-text" onClick={handleOnClick}>
+        <div>{props.text}</div>
+        {open ? <ArrowDropUp/> : <ArrowDropDown/>}
+      </div>
+      <Collapse in={open}>
+        {props.children}
+      </Collapse>
+    </Box>
+  );
+}
