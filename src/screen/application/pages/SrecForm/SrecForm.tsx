f7265c474a4bfd83032c87d13ebed5d537598d66
TimothyWang56 <timothywang56@gmail.com>
2021-02-01 20:04:10 -0500

    add srec page

--- src/screen/application/pages/SrecForm/SrecForm.tsx
+++ src/screen/application/pages/SrecForm/SrecForm.tsx
@@ -1,14 +1,34 @@
 import {ReactElement} from "react";
+import './SrecForm.scss';
 
 import {Box} from "@material-ui/core";
 
 import MaterialHeader from "../../components/MaterialHeader/MaterialHeader";
+import FormSelect from "../../components/FormSelect/FormSelect";
+import { createStringSlice, useReduxGetSet } from "../../Utils";
 
+// Redux string slices
+export const [
+    srecPaymentsSlice
+] = [
+    "srecPayments"
+].map(createStringSlice)
 
 export default function SrecForm(): ReactElement {
+    const srecPayments = useReduxGetSet<string>("srecPayments", srecPaymentsSlice);
+
     return (
         <Box>
             <MaterialHeader text={"SREC Payments"}/>
+            <Box className={"srec-form-container"}>
+            <FormSelect
+                label={"SREC Payments"}
+                value={srecPayments}
+                options={[
+                    "Up-front Payment",
+                    "Production-based Payments",
+                ]}/>
+            </Box>
         </Box>
     );
 }
