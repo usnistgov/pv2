7e3b1c0e67fb4b3907a3e7f329f4aca794d284e4
TimothyWang56 <timothywang56@gmail.com>
2021-02-15 00:48:20 -0500

    fix header and footer

--- src/screen/components/PageWrapper.tsx
+++ src/screen/components/PageWrapper.tsx
@@ -1,16 +1,24 @@
-import React, { PropsWithChildren } from "react";
-import useScript from "../hooks/useScript";
+import React, { PropsWithChildren, useState } from "react";
+import Script from "react-load-script";
 
 interface PageWrapperProps {
 }
 
 const PageWrapper = (props: PropsWithChildren<PageWrapperProps>) => {
-
-  useScript("https://pages.nist.gov/nist-header-footer/js/jquery-1.9.0.min.js");
-  useScript("https://pages.nist.gov/nist-header-footer/js/nist-header-footer.js");
-
   return (
     <div>
+      <Script
+        url="https://pages.nist.gov/nist-header-footer/js/jquery-1.9.0.min.js"
+        onCreate={() => undefined}
+        onError={() => undefined}
+        onLoad={() => undefined}
+      />
+      <Script
+        url="https://pages.nist.gov/nist-header-footer/js/nist-header-footer.js"
+        onCreate={() => undefined}
+        onError={() => undefined}
+        onLoad={() => undefined}
+      />
       <link rel="stylesheet" type="text/css" href={"https://pages.nist.gov/nist-header-footer/css/nist-combined.css"} />
       {props.children}
     </div>
