f6315567b8359af515ac15c3a5af14573915c49b
TimothyWang56 <timothywang56@gmail.com>
2021-03-15 19:18:05 -0400

    add unique key

--- src/screen/application/components/FormSelect/FormSelect.tsx
+++ src/screen/application/components/FormSelect/FormSelect.tsx
@@ -38,7 +38,7 @@ export default function FormSelect({label, value, options, required}: FormSelect
                     onChange={(event) => {
                         value.set(event.target.value as string)
                     }}>
-                {options.map(option => <MenuItem value={option}>{option}</MenuItem>)}
+                {options.map((option, i) => <MenuItem value={option} key={i}>{option}</MenuItem>)}
             </Select>
         </FormControl>
     );
