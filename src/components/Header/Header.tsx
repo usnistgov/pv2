d62fad707943dffa28d06e8a1a0ff82226b92dc8
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-06-02 14:09:41 -0400

    Added temporary files for the tutorial and user guide pdfs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,23 @@
-import { useHistory } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./Header.sass";
 
 const Header = () => {
-  const history = useHistory()
-
-  return (
-    <div className="header-wrapper">
-      <div className="options">
-        <div className="menu-option" onClick={() => history.push("/")}>
-          Home
+    return (
+        <div className="header-wrapper">
+            <div className="options">
+                <Link className={"menu-option"} to={"/"}>
+                    Home
+                </Link>
+                <a className={"menu-option"} href={"docs/user-guide.pdf"}>
+                    User Guide
+                </a>
+                <a className={"menu-option"} href={"docs/tutorial.pdf"}>
+                    Tutorial
+                </a>
+            </div>
+            <hr/>
         </div>
-        <div className="menu-option" onClick={() => history.push("/")}>
-          User Guide
-        </div>
-        <div className="menu-option" onClick={() => history.push("/")}>
-          Tutorial
-        </div>
-      </div>
-      <hr />
-    </div>
-  );
+    );
 }
 
 export default Header;
