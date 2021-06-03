4a44d464bc153c60473b400b10cb9650a7f52876
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-06-03 12:01:16 -0400

    Added value ranges for analysis assumption form

--- src/application/pages/AnalysisAssumptionsForm/AnalysisAssumptionsForm.tsx
+++ src/application/pages/AnalysisAssumptionsForm/AnalysisAssumptionsForm.tsx
@@ -15,10 +15,10 @@ import "../Form.sass";
 
 export default function AnalysisAssumptionsForm(): ReactElement {
     // Analysis Assumptions
-    const residualValueApproach = useReduxGetSet<string>("residualValueApproach", "");
+    const residualValueApproach = useReduxGetSet<string>("residualValueApproach", "Linear Depreciation");
     const studyPeriod = useReduxGetSet<number>("studyPeriod", 25);
-    const realDiscountRate = useReduxGetSet<number>("realDiscountRate", 0);
-    const generalInflation = useReduxGetSet<number>("generalInflation", 0);
+    const realDiscountRate = useReduxGetSet<number>("realDiscountRate", 20);
+    const generalInflation = useReduxGetSet<number>("generalInflation", 10);
 
     return (
         <Box className={"form-page-container"}>
@@ -26,19 +26,19 @@ export default function AnalysisAssumptionsForm(): ReactElement {
             <Box className={"form-single-column-container"}>
                 <FormField
                     label={"Study Period"}
-                    schema={Yup.number().required()}
+                    schema={Yup.number().required().max(40).min(1)}
                     value={studyPeriod}
                     endAdornment={"Years"}
                     type={"number"}/>
                 <FormField
                     label={"Real Discount Rate"}
-                    schema={Yup.number().required()}
+                    schema={Yup.number().required().max(100).min(0)}
                     value={realDiscountRate}
                     endAdornment={"%"}
                     type={"number"}/>
                 <FormField
                     label={"General Inflation Rate"}
-                    schema={Yup.number().required()}
+                    schema={Yup.number().required().max(100).min(0)}
                     value={generalInflation}
                     endAdornment={"%"}
                     type={"number"}/>
