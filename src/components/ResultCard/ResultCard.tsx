52431bb42b353b1da45a202b8e4c54037885e760
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-09-27 09:53:39 -0400

    Fixed: SPP now reports correct value

--- src/components/ResultCard/ResultCard.tsx
+++ src/components/ResultCard/ResultCard.tsx
@@ -77,7 +77,7 @@ export default function ResultCard({alt}: ResultCardProps): ReactElement {
                             </Pv2Tooltip>
                         </Grid>
                         <Grid item xs={6}>
-                            <div>{valid(alt.SPP) && alt.SPP !== "Infinity" ? decimals(alt.SPP, 5) : "NA"}</div>
+                            <div>{valid(alt.SPP) && alt.SPP !== "Infinity" ? `${decimals(alt.SPP, 2)}yr` : "NA"}</div>
                         </Grid>
 
                         <Grid item xs={6}>
