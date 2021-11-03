984a738a79b8baa66da1c48483b276685c6662cf
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-11-03 13:52:12 -0400

    Fix error message

--- frontend/src/components/ErrorDialog.tsx
+++ frontend/src/components/ErrorDialog.tsx
@@ -9,26 +9,39 @@ interface ErrorDialogProps {
     errorDetails: any;
 }
 
+const ErrorMessages: {[key: string]: string} = {
+    "0": '',
+    "400": "Input was not accepted. Click 'Show Details' to view the error returned by the server.",
+    "500": "An error occurred within the server. Please contact the system administrator."
+}
+
 export default function ErrorDialog({predicate, error, errorDetails}: ErrorDialogProps) {
     const [showErrorDetails, setShowErrorDetails] = useState(false);
     const [closed, setClosed] = useState(false);
+
+    const status: string = error?.response?.status ?? '0';
 
     return (
         <Dialog open={predicate && !closed} onClose={() => setClosed(true)}>
             <DialogTitle>An error has occurred</DialogTitle>
             <DialogContent>
                 <div>
-                    {`${error?.response?.status} ${error?.response?.statusText}`}
+                    {`${status} ${error?.response?.statusText}`}
                 </div>
-                {showErrorDetails && errorDetails && <code>
-                    {JSON.stringify(errorDetails)}
-                </code>}
+                <div>
+                    {ErrorMessages[status] ?? ''}
+                </div>
+                <div>
+                    {showErrorDetails && errorDetails && <pre>
+                        {JSON.stringify(errorDetails)}
+                    </pre>}
+                </div>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => setShowErrorDetails(!showErrorDetails)} color={"primary"}>
-                    Show Details
+                    {`${showErrorDetails ? 'Hide' : 'Show'} Details`}
                 </Button>
-                <Button onClick={() => setClosed(false)} color={"primary"}>
+                <Button onClick={() => setClosed(true)} color={"primary"}>
                     Close
                 </Button>
             </DialogActions>
