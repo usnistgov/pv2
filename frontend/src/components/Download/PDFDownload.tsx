19b433f95c2a47376014c3418bd2552d31a403f2
Luke Donmoyer <lukedonmoyer@gmail.com>
2021-11-22 15:18:30 -0500

    Graph now renders correctly on button click

--- frontend/src/components/Download/PDFDownload.tsx
+++ frontend/src/components/Download/PDFDownload.tsx
@@ -1,11 +1,11 @@
-import React, {useCallback, useContext, useState} from "react";
+import React, {useCallback, useContext} from "react";
 import {Button} from "@material-ui/core";
 import {Icon as MdiIcon} from "@mdi/react";
 import {mdiDownload} from "@mdi/js";
 import html2canvas from "html2canvas";
 import PdfGraph from "../PDF/components/PdfGraph";
 import PdfReport from "../PDF/PdfReport";
-import {PDFDownloadLink} from "@react-pdf/renderer";
+import {pdf} from "@react-pdf/renderer";
 import {Store} from "../../application/ApplicationStore";
 
 interface PDFDownloadProps {
@@ -18,7 +18,7 @@ interface PDFDownloadProps {
  * @param result The results to generate a PDF for.
  */
 export default function PDFDownload({result}: PDFDownloadProps) {
-    const [graphSrc, setGraphSrc] = useState("");
+    const store = useContext(Store);
 
     const generatePdf = useCallback(() => {
         const pdfGraph = document.getElementById("pdf-graph");
@@ -26,22 +26,31 @@ export default function PDFDownload({result}: PDFDownloadProps) {
         if (pdfGraph == null)
             return;
 
-        html2canvas(pdfGraph).then(canvas => {
-            setGraphSrc(canvas.toDataURL("image/png"));
-        });
+        html2canvas(pdfGraph)
+            .then(canvas => canvas.toDataURL("image/png"))
+            .then(async graphSrc => {
+                return pdf(<PdfReport graphSrc={graphSrc} result={result} store={store}/>).toBlob()
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+
+                const link = document.createElement("a");
+                link.href = url;
+                link.download = "PV2 Analysis Report.pdf";
+
+                link.click();
+            })
     }, []);
 
     return (
         <>
             <PdfGraph result={result}/>
-            <PDFDownloadLink document={<PdfReport graphSrc={graphSrc} result={result} store={useContext(Store)}/>}>
-                <Button variant={"contained"}
-                        color={"primary"}
-                        startIcon={<MdiIcon path={mdiDownload} size={1}/>}
-                        onClick={generatePdf}>
-                    PDF
-                </Button>
-            </PDFDownloadLink>
+            <Button variant={"contained"}
+                    color={"primary"}
+                    startIcon={<MdiIcon path={mdiDownload} size={1}/>}
+                    onClick={generatePdf}>
+                PDF
+            </Button>
         </>
     );
 }
