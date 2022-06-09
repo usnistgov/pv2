ab30466c6b8eb91b7473cd7db40931ff7cd241a1
Donmoyer <lrd@NIST.GOV>
2022-06-09 09:58:25 -0400

    Removed Google Maps component

--- frontend/src/application/pages/AddressForm/AddressForm.tsx
+++ frontend/src/application/pages/AddressForm/AddressForm.tsx
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 
 // Library Imports
-import {Grid} from "@material-ui/core";
+import {Box, Grid} from "@material-ui/core";
 import * as Yup from "yup";
 import {observer} from "mobx-react-lite";
 import {action} from "mobx";
@@ -23,10 +23,6 @@ import ValidatedTextField from "../../../components/ValidatedTextField";
 import Info from "../../../components/Info/Info";
 import ResetButton from "../../../components/ResetButton/ResetButton";
 
-// Stylesheets
-import "./AddressForm.sass"
-import GoogleMap from "../../../components/GoogleMap/GoogleMap";
-
 /*
  * The AddressForm component is the first page of the application form that lets user fill
  * in their address and displays an embedded iframe of Google Maps with the inputted location.
@@ -34,68 +30,49 @@ import GoogleMap from "../../../components/GoogleMap/GoogleMap";
 const AddressForm = observer(() => {
     const store = useContext(Store).addressFormStore;
 
-    // Google Maps embedded url query
-    const wholeMap = !(store.address || store.city || store.state || store.zipcode) ? "&center=39.8097343,-98.5556199&zoom=3" : "";
-    const query = `${store.address}, ${store.city}, ${store.state} ${store.zipcode}${wholeMap}`;
-
     return (
-        <>
+        <Box className={"form-page-container"}>
             <MaterialHeader text={"Address"} right={<ResetButton onClick={() => store.reset()}/>}/>
-            <div className={"address-form-text"}>
+            <div className={"form-page-text"}>
                 Insert address for home on which a solar PV system may be installed.
                 Only the ZIP Code is necessary for [PV]<sup>2</sup> to calculate results.
             </div>
-            <Grid className={"address-form-container"} container justifyContent={"center"} spacing={8}>
-                <Grid item xs={6}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Info tooltip={ADDRESS_TOOLTIP}>
-                                <ValidatedTextField fullWidth
-                                                    label={ADDRESS_LABEL}
-                                                    value={store.address}
-                                                    variant={"filled"}
-                                                    schema={Yup.string()}
-                                                    onValidate={action((value) => store.address = value)}/>
-                            </Info>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Info tooltip={CITY_TOOLTIP}>
-                                <ValidatedTextField fullWidth
-                                                    label={CITY_LABEL}
-                                                    value={store.city}
-                                                    variant={"filled"}
-                                                    schema={Yup.string()}
-                                                    onValidate={action((value) => store.city = value)}/>
-                            </Info>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Info tooltip={STATE_TOOLTIP}>
-                                <ValidatedTextField fullWidth
-                                                    label={STATE_LABEL}
-                                                    value={store.state}
-                                                    variant={"filled"}
-                                                    schema={Yup.string()}
-                                                    onValidate={action((value) => store.state = value)}/>
-                            </Info>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Info tooltip={ZIPCODE_TOOLTIP}>
-                                <ValidatedTextField fullWidth
-                                                    required
-                                                    label={ZIPCODE_LABEL}
-                                                    value={store.zipcode}
-                                                    variant={"filled"}
-                                                    schema={Yup.string().required()}
-                                                    onValidate={action((value) => store.zipcode = value)}/>
-                            </Info>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item xs={6}>
-                    <GoogleMap query={query}/>
-                </Grid>
-            </Grid>
-        </>
+            <Box className={"form-single-column-container"}>
+                <Info tooltip={ADDRESS_TOOLTIP}>
+                    <ValidatedTextField fullWidth
+                                        label={ADDRESS_LABEL}
+                                        value={store.address}
+                                        variant={"filled"}
+                                        schema={Yup.string()}
+                                        onValidate={action((value) => store.address = value)}/>
+                </Info>
+                <Info tooltip={CITY_TOOLTIP}>
+                    <ValidatedTextField fullWidth
+                                        label={CITY_LABEL}
+                                        value={store.city}
+                                        variant={"filled"}
+                                        schema={Yup.string()}
+                                        onValidate={action((value) => store.city = value)}/>
+                </Info>
+                <Info tooltip={STATE_TOOLTIP}>
+                    <ValidatedTextField fullWidth
+                                        label={STATE_LABEL}
+                                        value={store.state}
+                                        variant={"filled"}
+                                        schema={Yup.string()}
+                                        onValidate={action((value) => store.state = value)}/>
+                </Info>
+                <Info tooltip={ZIPCODE_TOOLTIP}>
+                    <ValidatedTextField fullWidth
+                                        required
+                                        label={ZIPCODE_LABEL}
+                                        value={store.zipcode}
+                                        variant={"filled"}
+                                        schema={Yup.string().required()}
+                                        onValidate={action((value) => store.zipcode = value)}/>
+                </Info>
+            </Box>
+        </Box>
     );
 })
 
