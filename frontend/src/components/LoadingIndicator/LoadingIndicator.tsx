ffbe4e4a3b6921794471d5364dc5a1f13a15d82e
Luke Donmoyer <luke.donmoyer@nist.gov>
2022-08-09 09:38:54 -0400

    Change cancel button to use navigate(-1) instead of a link

--- frontend/src/components/LoadingIndicator/LoadingIndicator.tsx
+++ frontend/src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -3,21 +3,22 @@ import {Icon as MdiIcon} from "@mdi/react";
 import {mdiClose} from "@mdi/js";
 import Constants from "../../Constants";
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 interface LoadingIndicatorProps {
     predicate: boolean;
 }
 
 export default function LoadingIndicator({predicate}: LoadingIndicatorProps) {
+    const navigate = useNavigate();
+
     return (
         <Backdrop className={"result-loading-backdrop"} open={predicate}>
             <Box className={"loading-indicator"}>
                 <CircularProgress/>
                 <h1>Calculating Results</h1>
                 <Button className={"cancel-calculation-button"}
-                        component={Link}
-                        to={Constants.routes.APPLICATION}
+                        onClick={() => navigate(-1)}
                         variant={"contained"}
                         color={"secondary"}
                         startIcon={<MdiIcon path={mdiClose} size={1}/>}>
